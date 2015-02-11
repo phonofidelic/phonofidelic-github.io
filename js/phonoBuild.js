d669ddff00db003613bc143804c38244cfd8c37f
phonofidelic <chris_clemons85@hotmail.com>
2015-02-11 03:02:51 -0500

    Added js, worked on index, changed some immage names

---
+++ js/phonoBuild.js
@@ -0,0 +1,22 @@
+var projects = {
+	'thumbs' : ['p1_s', 'p2_s', 'p3_s', 'p4_s', 'p5_s', 'p6_s', 'p7_s', 'p8_s', 'p9_s', 'p10_s', 'p11_s', 'p12_s', 'p13_s', 'p14_s', 'p15_s', 'p16_s'],
+
+	displayThumbs : function() {
+		$("#project-thumbs").append(HTMLprojectStart);
+
+		for (thumb in projects.thumbs) {
+
+			var formattedThumb = HTMLprojectThumb.replace("%data%", projects.thumbs[thumb]);
+			$(".project-entry").append(formattedThumb);
+		}
+	}
+};
+projects.displayThumbs();
+
+// var projectItem = {
+// 	"id":
+// 	"title" :
+// 	"image" : 
+// 	"description":
+// 	"footerNav" : 
+// };
---
+++ js/phonoHelper.js
@@ -0,0 +1,3 @@
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<h2>Projects</h2>';
+var HTMLprojectThumb = '<div class="col-md-1" class="thumb"><img src="images/%data%.jpg" alt="Thumbnail image"></div>';
