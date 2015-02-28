32ed4ae7ec751e043ae9314a994b3b63a77ede02
phonofidelic <chris_clemons85@hotmail.com>
2015-02-28 07:35:50 -0500

    Added responsive class to modal images

--- js/phonoHelper.js
+++ js/phonoHelper.js
@@ -3,7 +3,7 @@ var HTMLprojectTitle = '<h2>Projects</h2>';
 var HTMLprojectThumb = '<div class="col-xs-1 col-sm-1 col-md-1"><img src="images/%modalImage%.jpg" data-toggle="modal" data-target="%modalId%" class="thumb"></div>';
 
 var HTMLmodalStart = '<div class="modal-entry">';
-var HTMLmodal = '<div class="modal fade" id="%modalLink%" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">"%modalTitle%"</h4></div><div class="modal-body"><img src="%modalImage%"></div><div class="modal-footer"></div></div></div></div>';
+var HTMLmodal = '<div class="modal fade" id="%modalLink%" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">"%modalTitle%"</h4></div><div class="modal-body"><img src="%modalImage%" class="img-responsive"></div><div class="modal-footer"></div></div></div></div>';
 
 /*var HTMLprojectNavNext = '<a href="%nextProj%" data-target="%modalId%"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>';
 var HTMLprojectNavPrev = '<a href="%prevProj%"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>';*/
