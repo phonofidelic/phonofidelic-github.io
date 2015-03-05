4c8075af9357d5bd8fb83349c0d8a0e5d980fd0e
phonofidelic <chris_clemons85@hotmail.com>
2015-03-05 14:36:53 -0500

    Added responsive layout to screens < 2400px in width. Trying different background options.

--- js/media_query.js
+++ js/media_query.js
@@ -1,7 +1,25 @@
-if (window.matchMedia("(min-width: 2400px)").matches) {
-  /* the view port is at least 2400 pixels wide */
-  $( "div.container" ).replaceWith( ".container-fluid" );
+// if (window.matchMedia("(min-width: 2400px)").matches) {
+//   /* the view port is at least 2400 pixels wide */
+//   console.log("something");
+//   $( "div.container" ).replaceWith( ".container-fluid" );
+// } else {
+//   /* the view port is less than 2400 pixels wide */
+//   console.log("something");
+// }
+
+
+var mq = window.matchMedia('@media all and (max-width: 2400px)');
+if(mq.matches) {
+    // the width of browser is more then 2400px
+    console.log(test);
 } else {
-  /* the view port is less than 2400 pixels wide */
-  console.log("something");
+    // the width of browser is less then 2400px
 }
+
+mq.addListener(function(changed) {
+    if(changed.matches) {
+        // the width of browser is more then 2400px
+    } else {
+        // the width of browser is less then 2400px
+    }
+});
