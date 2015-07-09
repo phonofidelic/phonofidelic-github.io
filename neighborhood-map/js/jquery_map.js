157afc3c25c80d82e23ca2e03daf4d7770e195a5
phonofidelic <chris_clemons85@hotmail.com>
2015-07-09 16:53:32 -0400

    removed neighbourhood_map directory

--- neighborhood-map/js/jquery_map.js
+++
@@ -1,8 +0,0 @@
-$('#map_canvas').gmap({'options'}).bind('init', function() {
-	for (var i = 0; i < DataModel.foodlist().length; i++) {
-		$('#map_canvas').gmap('addMarker', {'posision': loc, 'image': 'img/white-pin.png'}).click(function() {
-			$('#map_canvas').gmap('openInfoWindow', {'content': 'TEST'}, this);
-			// marker image = img/red-pin.png
-		});
-	}
-});
